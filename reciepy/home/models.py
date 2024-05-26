e27a0eee5e815fc95e0b211c172a01b5315419b4
Vishal <vishalpal1501@gmail.com>
2024-05-26 15:52:35 +0530

    database migration and basic setup done

--- reciepy/home/models.py
+++ reciepy/home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Reciepy(models.Model):
+    receipy_name=models.CharField(max_length=100)
+    receipy_des=models.TextField()
+    receipy_img=models.ImageField(upload_to="receipeimage")
--- reciepy/home/views.py
+++ reciepy/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 def home(request):
     print(request)
-    return HttpResponse("Hello welcome back home")
+    return render(request,"index.html")
