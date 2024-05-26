69a9664daafbb6131b06d4def495299c883c53ee
Vishal <vishalpal1501@gmail.com>
2024-05-26 17:13:10 +0530

    In this commit we done how to send form data to backend and store in the database

--- reciepy/home/views.py
+++ reciepy/home/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import *
 # Create your views here.
 def home(request):
-    print(request)
+    if request.method== "POST" :
+        data=request.POST
+        receipy_img=request.FILES.get("receipy_img")
+        receipy_name=data.get("receipy_name")
+        receipy_des=data.get("receipy_des")
+        Reciepy.objects.create(
+            receipy_img=receipy_img,
+            receipy_name=receipy_name,
+            receipy_des=receipy_des
+        )
+
+        return redirect("/home/")
     return render(request,"index.html")
